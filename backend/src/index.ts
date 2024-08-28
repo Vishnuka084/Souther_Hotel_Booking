ed603bef133a9c5387a37e635af003834d30a580
vishnuk084 <vishnukayahan84@gmail.com>
2024-08-28 13:52:22 +0530

    connect mongodb

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,33 +1,18 @@
-// import express, { Request, Response } from 'express';
-// import cors from 'cors';
-// import "dotenv/config";
-
-// const app = express();
-// app.use(express.json());
-// app.use(express.urlencoded({extended: true}));
-// app.use(cors());
-
-// app.get("/api/test", async (req: Request, res: Response ) => {
-//     res.json({ message: "Hello from express endpoint "});
-// });
-
-// app.listen(5000, () => {
-//     console.log("server running on localhost: 5000");
-// });
-
-
 import express, { Request, Response } from 'express';
 import cors from 'cors';
 import "dotenv/config";
+import mongoose from 'mongoose';
+
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.get("/api/test", async (req: Request, res: Response) => {
-    res.json({ message: "Hello from express endpoint" });
-});
+// app.get("/api/test", async (req: Request, res: Response) => {
+//     res.json({ message: "Hello from express endpoint" });
+// });
 
 const PORT = process.env.PORT || 5001;  // Changed from 5000 to 5001
 app.listen(PORT, () => {
