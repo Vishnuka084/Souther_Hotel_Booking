4297bdafd0eaa70ac1611521eced1d5466116da2
vishnuk084 <vishnukayahan84@gmail.com>
2024-09-11 18:01:48 +0530

    add model and routh  file

--- backend/src/models/hotel.ts
+++ backend/src/models/hotel.ts
@@ -1,21 +1,5 @@
 import mongoose from "mongoose";
-
-export type HotelType = {
-    _id: string;
-    userId: string;
-    name: string;
-    city: string;
-    country: string;
-    description: string;
-    type: string;
-    adultCount: number;
-    childCount: number;
-    facilities: string[];
-    pricePerNight: number;
-    starRating: number;
-    imageUrls: string[];
-    lastUpdated: Date;
-  };
+import { UserType, HotelType } from "../shared/types";
 
   const hotelSchema = new mongoose.Schema<HotelType>({
     userId: { type: String, required: true },
@@ -32,3 +16,7 @@ export type HotelType = {
     imageUrls: [{ type: String, required: true }],
     lastUpdated: { type: Date, required: true },
   });
+
+
+const Hotel = mongoose.model<HotelType>("Hotel", hotelSchema);
+export default Hotel;
