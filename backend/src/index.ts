3b2cb14d9f9386613a96bf586d378ae2e3a556cd
vishnuk084 <vishnukayahan84@gmail.com>
2024-09-12 15:34:03 +0530

    Updated Contete

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,6 +7,7 @@ import authRoutes from "./routes/auth";
 import cookieParser from 'cookie-parser';
 import path from 'path';
 import { v2 as cloudinary } from "cloudinary";
+import myHotelRoutes from './routes/my-hotels';
 
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
@@ -40,6 +41,7 @@ app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes );
+app.use("/api/my-hotels", myHotelRoutes)
 
 const PORT = process.env.PORT || 5001;  // Changed from 5000 to 5001
 app.listen(PORT, () => {
