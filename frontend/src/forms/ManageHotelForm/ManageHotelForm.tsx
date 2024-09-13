0c6364466531d9483edaae8663b0303364cdcada
vishnuk084 <vishnukayahan84@gmail.com>
2024-09-13 14:28:05 +0530

    ManageHotelForm.tsx file updated

--- frontend/src/forms/ManageHotelForm/ManageHotelForm.tsx
+++ frontend/src/forms/ManageHotelForm/ManageHotelForm.tsx
@@ -1,8 +1,9 @@
 import { FormProvider, useForm } from "react-hook-form";
 import DetailsSection from "./DetailsSection";
 import TypeSection from "./TypeSection";
-import FacilitiesSection from "./FacilitiesScetion";
+import FacilitiesSection from "./FacilitiesSection";
 import GuestsSection from "./GuestsSection";
+import ImagesSection from "./ImagesSection";
 
 export type HotelFormData = {
   name: string;
@@ -20,13 +21,29 @@ export type HotelFormData = {
 
 const ManageHotelForm = () => {
   const formMethods = useForm<HotelFormData>();
+  const { handleSubmit } = formMethods;
+
+  const onSubmit = handleSubmit((formData: HotelFormData) => {
+    //create new formData object & call our Api
+    console.log(formData);
+  });
+
   return (
     <FormProvider {...formMethods}>
-      <form className="flex flex-col gap-10">
+      <form className="flex flex-col gap-10" onSubmit={onSubmit}>
         <DetailsSection />
         <TypeSection />
         <FacilitiesSection />
         <GuestsSection />
+        <ImagesSection />
+        <span className="flex justify-end">
+          <button
+            type="submit"
+            className="bg-blue-600 text-white p-2 font-bold hover:bg-blue-500 text-xl disabled:bg-gray-500"
+          >
+            save
+          </button>
+        </span>
       </form>
     </FormProvider>
   );
