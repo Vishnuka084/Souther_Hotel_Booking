583b3520b3669e03c6d893b373e81776f0f0f352
vishnuk084 <vishnukayahan84@gmail.com>
2024-09-14 01:01:45 +0530

    Update Contnet  api client

--- frontend/src/api-client.ts
+++ frontend/src/api-client.ts
@@ -58,4 +58,14 @@ export const signOut = async () => {
     }
   };
 
-  
+export const addMyHotel = async (hotelFormData: FormData) => {
+    const response = await fetch(`${API_BASE_URL}/api/my-hotels`, {
+        credentials: "include",
+        method: "POST",
+        body: hotelFormData,
+      });
+
+    if(!response.ok) {
+        throw new Error("Failed to add Hotel")
+    }
+}
