47c0b252e3ff6c7b7fc4ad4b770a60f49c4058e7
vishnuk084 <vishnukayahan84@gmail.com>
2024-09-14 00:56:22 +0530

    Updated Content

--- frontend/src/pages/AddHotel.tsx
+++ frontend/src/pages/AddHotel.tsx
@@ -1,7 +1,27 @@
+import { useMutation } from "react-query";
 import ManageHotelForm from "../forms/ManageHotelForm/ManageHotelForm";
+import { useAppContext } from "../contexts/AppContext";
 
 const AddHotel = () => {
-  return <ManageHotelForm />;
+  const { showToast } = useAppContext();
+
+  const { mutate, isLoading } = useMutation(apiClient.addMyHotel, {
+    onSuccess: () => {
+      showToast({ message: "Hotel Saved!", type: "SUCCESS" });
+    },
+    onError: () => {
+      showToast({
+        message: "Error saving Hotel!",
+        type: "ERROR"
+      });
+    }
+  });
+
+  const handleSave = (hotelFormData: FormData) => {
+    mutate(hotelFormData);
+  };
+
+  return <ManageHotelForm onSave={handleSave} />;
 };
 
 export default AddHotel;
