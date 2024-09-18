9062da33ef72d110fbc4d59c9ae487d2aeadebf2
vishnuk084 <vishnukayahan84@gmail.com>
2024-09-19 00:29:27 +0530

    Updated Content

---
+++ frontend/src/pages/Search.tsx
@@ -0,0 +1,36 @@
+import { useQuery } from "react-query";
+import { useSearchContext } from "../contexts/SearchContext"
+import * as apiClient from "../api-client";
+import { useState } from "react";
+
+const Search = () => {
+    const search = useSearchContext();
+    const [page, setPage] = useState<number>(1);
+    const [selectedStars, setSelectedStars] = useState<string[]>([]);
+    const [selectedHotelTypes, setSelectedHotelTypes] = useState<string[]>([]);
+    const [selectedFacilities, setSelectedFacilities] = useState<string[]>([]);
+    const [selectedPrice, setSelectedPrice] = useState<number | undefined>();
+    const [sortOption, setSortOption] = useState<string>("");
+
+    const searchParams = {
+        destination: search.destination,
+        checkIn: search.checkIn.toISOString(),
+        checkOut: search.checkOut.toISOString(),
+        adultCount: search.adultCount.toString(),
+        childCount: search.childCount.toString(),
+        page: page.toString(),
+        stars: selectedStars,
+        types: selectedHotelTypes,
+        facilities: selectedFacilities,
+        maxPrice: selectedPrice?.toString(),
+        sortOption,
+      };
+    
+
+    const { data: hotelData } = useQuery(["searchHotels", searchParams], () =>
+        apiClient.searchHotels(searchParams)
+      );
+    return <>Search Page!</>
+}
+
+export default  Search;
