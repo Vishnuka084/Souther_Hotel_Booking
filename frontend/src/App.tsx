d1fee0fa37032c7fa2bd1c01c9329dc14d0a071b
vishnuk084 <vishnukayahan84@gmail.com>
2024-09-18 10:23:36 +0530

    Updaeted Content

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,6 +10,7 @@ import SignIn from "./pages/SignIn";
 import { useAppContext } from "./contexts/AppContext";
 import AddHotel from "./pages/AddHotel";
 import MyHotels from "./pages/MyHotels";
+import EditHotel from "./pages/EditHotel";
 
 const App = () => {
   const { isLoggedIn } = useAppContext();
