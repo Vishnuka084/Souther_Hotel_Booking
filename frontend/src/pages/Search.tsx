ea47dc4e6419f0fb9e53c1770e8b8cf03f4e73dd
vishnuk084 <vishnukayahan84@gmail.com>
2024-09-19 11:12:14 +0530

    Search bar function updated

--- frontend/src/pages/Search.tsx
+++ frontend/src/pages/Search.tsx
@@ -1,36 +1,44 @@
 import { useQuery } from "react-query";
-import { useSearchContext } from "../contexts/SearchContext"
+import { useSearchContext } from "../contexts/SearchContext";
 import * as apiClient from "../api-client";
 import { useState } from "react";
 
 const Search = () => {
-    const search = useSearchContext();
-    const [page, setPage] = useState<number>(1);
-    const [selectedStars, setSelectedStars] = useState<string[]>([]);
-    const [selectedHotelTypes, setSelectedHotelTypes] = useState<string[]>([]);
-    const [selectedFacilities, setSelectedFacilities] = useState<string[]>([]);
-    const [selectedPrice, setSelectedPrice] = useState<number | undefined>();
-    const [sortOption, setSortOption] = useState<string>("");
+  const search = useSearchContext();
+  const [page, setPage] = useState<number>(1);
 
-    const searchParams = {
-        destination: search.destination,
-        checkIn: search.checkIn.toISOString(),
-        checkOut: search.checkOut.toISOString(),
-        adultCount: search.adultCount.toString(),
-        childCount: search.childCount.toString(),
-        page: page.toString(),
-        stars: selectedStars,
-        types: selectedHotelTypes,
-        facilities: selectedFacilities,
-        maxPrice: selectedPrice?.toString(),
-        sortOption,
-      };
-    
+  const searchParams = {
+    destination: search.destination,
+    checkIn: search.checkIn.toISOString(),
+    checkOut: search.checkOut.toISOString(),
+    adultCount: search.adultCount.toString(),
+    childCount: search.childCount.toString(),
+    page: page.toString()
+  };
 
-    const { data: hotelData } = useQuery(["searchHotels", searchParams], () =>
-        apiClient.searchHotels(searchParams)
-      );
-    return <>Search Page!</>
-}
+  const { data: hotelData } = useQuery(["searchHotels", searchParams], () =>
+    apiClient.searchHotels(searchParams)
+  );
 
-export default  Search;
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-[250px_1fr] gap-5">
+      <div className="rounded-lg border border-slate-300 p-5 h-fit sticky top-10">
+        <div className="space-y-5">
+          <h3 className="text-lg font-semibold border-b border-slate-300 pb-5">
+            Filter by:
+          </h3>
+        </div>
+      </div>
+      <div className="flex flex-col gap-5">
+        <div className="flex justify-between items-center">
+          <span className="text-xl font-bold">
+            {hotelData?.pagination.total} Hotels found
+            {search.destination ? ` in ${search.destination}` : ""}
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Search;
