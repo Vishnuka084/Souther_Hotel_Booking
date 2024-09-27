860d14abd19c6bcd3c94d2fc81c39a5030c95edd
vishnuk084 <vishnukayahan84@gmail.com>
2024-09-27 12:15:27 +0530

    Updated Form ui design

--- frontend/src/pages/Detail.tsx
+++ frontend/src/pages/Detail.tsx
@@ -2,6 +2,7 @@ import { useQuery } from "react-query";
 import { useParams } from "react-router-dom";
 import * as apiClient from "./../api-client";
 import { AiFillStar } from "react-icons/ai";
+import GuestInfoForm from "../forms/GuestInfoForm/GuestInfoForm";
 
 
 const Detail = () => {
